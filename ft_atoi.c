4ea61f05bb557eb52534a3083065062b574c2a13
mnaschbe <133028123+mnaschbe@users.noreply.github.com>
2024-03-08 19:33:18 +0100

    ft_atoi.c aktualisieren

---
+++ ft_atoi.c
@@ -0,0 +1,16 @@
+#include<unistd.h>
+#include<stdio.h>
+
+int ft_atoi(char *s)
+{
+    int i= 0;
+    int sign = 1;
+    int nb;
+    
+    while(s[i] == '\t' || s[i] == '\n' || s[i] == 32)
+    i++;
+    if (s[i] == 45)
+        sign = sign * -1;
+    else if (s[i] == 43)
+        i++;
+    while (s[
